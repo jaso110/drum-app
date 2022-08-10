40587562149d947d0a1fa8b4230f19e6fdcfc7e1
Jan Sørensen <jaso110@gmail.com>
2022-08-11 00:03:25 +0200

    update

---
+++ index.js
@@ -0,0 +1,35 @@
+document.querySelector(".w").addEventListener("click", clickedW);
+document.querySelector(".a").addEventListener("click", clickedA);
+document.querySelector(".s").addEventListener("click", clickedS);
+document.querySelector(".d").addEventListener("click", clickedD);
+document.querySelector(".j").addEventListener("click", clickedJ);
+document.querySelector(".k").addEventListener("click", clickedK);
+document.querySelector(".l").addEventListener("click", clickedL);
+
+function clickedW() {
+  alert("w");
+}
+
+function clickedA() {
+  alert("a");
+}
+
+function clickedS() {
+  alert("s");
+}
+
+function clickedD() {
+  alert("d");
+}
+
+function clickedJ() {
+  alert("j");
+}
+
+function clickedK() {
+  alert("k");
+}
+
+function clickedL() {
+  alert("l");
+}
